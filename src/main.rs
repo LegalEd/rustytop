7f5bc85275783b0b321d606dd885e0a214c98979
Christopher Elias <celias@costar.co.uk>
2024-04-16 10:14:52 +0100

    Adding pid path, user, cpu usage

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+use std::path::Path;
+
+use sysinfo::Uid;
+use users::{get_user_by_uid, get_current_uid};
+
+
+
 fn main(){
 
 use sysinfo::{
-    Components, Disks, Networks, System,
+    Components, Disks, Networks, System
 };
 
-// Please note that we use "new_all" to ensure that all list of
-// components, network interfaces, disks and users are already
-// filled!
+// "new_all" to ensure that all list of components, network interfaces,
+// disks and users are already filled!
 let mut sys = System::new_all();
+
+
+let user = get_user_by_uid(get_current_uid()).unwrap();
+println!("Hello, {}!", user.name().to_string_lossy());
 
 // Update all information of our `System` struct.
 sys.refresh_all();
@@ -26,15 +36,34 @@ println!("total swap  : {} bytes", sys.total_swap());
 println!("used swap   : {} bytes", sys.used_swap());
 
 
-
 // Number of CPUs:
 println!("#CPUs: {}", sys.cpus().len());
 
 // Display processes ID, name na disk usage:
 println!("=> processes");
+
+// container for processes
+
+let mut process_map: Vec<(u32, &str, Option<&Path>, Option<&Uid>, f32)> = Vec::new();
+
 for (pid, process) in sys.processes() {
-    println!("[{pid}] {}", process.name());
+    process_map.push((pid.as_u32(), process.name(), process.exe(), process.user_id(), process.cpu_usage()));
 }
+
+process_map.sort_by_key(|k| k.0);
+
+for (pid, process, path, owner, cpu_use) in process_map {
+    match owner {
+    Some(owner) => { println!("{}, {}, {:?}, {:?}, {:?}", pid, process, path.unwrap(), Some(get_user_by_uid(owner.to_string().parse::<u32>().unwrap()).unwrap().name()).unwrap(), cpu_use); },
+    None => { println!("{}, {}, {:?}, None, {:?}", pid, process, path.unwrap(), cpu_use); },
+}
+}
+
+// for (pid, process) in sys.processes() {
+//     process_map.push((pid.as_u32(), process.user_id()));
+// }
+
+
 
 // We display all disks' information:
 println!("=> disks:");
